5fa944b6c0e216f43af11350e9671b2db9022623
Vincenzo Edoardo Martino <vincenzo.martino@sectigo.com>
2020-07-22 23:23:11 -0400

    Added Product Service and root Injector

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -9,12 +10,16 @@ import { IProduct } from './product';
 export class ProductListComponent 
     implements OnInit {
     
-    
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     
+    // We create a separate property to bind filtered products.
+    // This prevents us keeping the original list of products unaffected by filtering operations.
+    filteredProducts: IProduct[];
+    products: IProduct[] = [];
+
     _listFilter: string;
     public get listFilter() : string {
         return this._listFilter;
@@ -24,37 +29,9 @@ export class ProductListComponent
         this._listFilter = v;
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
-    
-    
-    // We create a separate property to bind filtered products.
-    // This prevents us keeping the original list of products unaffected by filtering operations.
-    filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2019",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2019",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "assets/images/hammer.png"
-        }
-    ];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
+        // this.listFilter = 'cart';
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -66,8 +43,10 @@ export class ProductListComponent
         this.showImage = !this.showImage;
     }
 
+    // We use onInit to initialize the component with data
     ngOnInit(): void {
-        console.log('In OnInit()');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 
     onRatingClicked(event: string): void {
